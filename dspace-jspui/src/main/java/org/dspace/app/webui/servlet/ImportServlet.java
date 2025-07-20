07526f43f9121075b5c044ab8ea2edf286052eb2
Dyashin <dyashin36@gmail.com>
2025-07-20 12:41:28 +0400

    additive logs?

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/ImportServlet.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/ImportServlet.java
@@ -251,7 +251,7 @@ public class ImportServlet extends DSpaceServlet {
             NodeList testWow = doc.getElementsByTagName("m:BiblRecords");
             try{
                 if(testWow.getLength() > 0) {
-                    log.info("SH>>doDSPost>>BiblRecoreds number is more than 0");
+                    log.info("SH>>doDSPost>>BiblRecoreds number is more than 0; redirect to import");
                     request.getRequestDispatcher("/import/import-item.jsp").forward(request, response);
                 } else {
                     log.info("SH>>doDSPost>>BiblRecoreds number is lesser than 0");
