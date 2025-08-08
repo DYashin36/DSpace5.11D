e711a0d5450fc79613569f61d0a67bed0e259ea3
Dyashin <dyashin36@gmail.com>
2025-08-08 14:14:10 +0400

    fix Arrays import

--- dspace-api/src/main/java/org/dspace/app/itemexport/ItemExport.java
+++ dspace-api/src/main/java/org/dspace/app/itemexport/ItemExport.java
@@ -21,6 +21,7 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
